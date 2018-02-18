9cff2cbf1d2d70e707d194d0357fcda892c2618a
Marlon Hänsdieke <marlon.haensdieke@check24.de>
2018-02-18 15:37:00 +0100

    Add units test configuration

---
+++ src/app/app.component.spec.js
@@ -0,0 +1,18 @@
+describe('app-root component', function () {
+    beforeEach(angular.mock.module('app'));
+   
+    let element;
+    let scope;
+
+    beforeEach(inject(function ($rootScope, $compile) {
+      scope = $rootScope.$new();
+      element = angular.element('<app-root></app-root>');
+      element = $compile(element)(scope);
+    }));
+
+    it('should render the root component', function () {
+      let app = element.find('.app');
+console.log(app.length);
+      expect(app.length).toBe(1);      
+    });       
+});
